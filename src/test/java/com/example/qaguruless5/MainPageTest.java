eb69aed830c1af5d9ef8162b9e420525a74173b3
Калугин Александр Александрович <AKalugin2@alfabank.ru>
2023-03-03 17:16:02 +0100

    add negative case

--- src/test/java/com/example/qaguruless5/MainPageTest.java
+++ src/test/java/com/example/qaguruless5/MainPageTest.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPageTest {
@@ -23,12 +24,15 @@ public class MainPageTest {
         gh.enterpriseButton.click();
         gh.enterpriseContent.shouldHave(text("Build like the best"));
     }
-    @Test
+    @Test /*Failed*/
     public void drugndropFirst(){
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        actions().dragAndDrop(gh.colomnA, gh.colomnB); //не работает
+        //actions().dragAndDrop(gh.colomnA, gh.colomnB); //визуально никакие действия не наблюдаются, фигуры местами не меняются, ошибка при перемещении не возникает
+        //actions().dragAndDropBy(gh.colomnA, 250,0);//визуально никакие действия не наблюдаются, фигуры местами не меняются, ошибка при перемещении не возникает
+        //actions().clickAndHold(gh.colomnA).moveByOffset(250, 0).release().build().perform(); // переносимая фигура прилипает к оригинальному крусору
+        actions().clickAndHold(gh.colomnA).moveToElement(gh.colomnB).release().build().perform(); // переносимая фигура прилипает к оригинальному крусору
         gh.colomnA.shouldHave(text("B"));
-        sleep(1000); // слип для визуального контроля теста// слип для визуального контроля теста
+        sleep(1000); // слип для визуального контроля теста
     }
 
     @Test
@@ -36,7 +40,7 @@ public class MainPageTest {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         gh.colomnA.dragAndDropTo(gh.colomnB);// работает
         gh.colomnA.shouldHave(text("B"));
-        sleep(1000); // слип для визуального контроля теста// слип для визуального контроля теста
+        sleep(1000); // слип для визуального контроля теста
     }
 
 }
